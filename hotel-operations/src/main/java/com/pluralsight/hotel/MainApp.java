79b360b43da4cd75df22d11cdd4b066c7ccd5f51
Siry Siu <sewsiri@gmail.com>
2024-10-23 20:46:28 -0400

    made test files

--- hotel-operations/src/main/java/com/pluralsight/hotel/MainApp.java
+++ hotel-operations/src/main/java/com/pluralsight/hotel/MainApp.java
@@ -1,26 +1,30 @@
 package com.pluralsight.hotel;
 
 import javax.print.DocFlavor;
+import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
         Room room = new Room(2, 1200.82, true, true);
         Employee employee = new Employee("A1", "Joe mama", "Front Desk", 15.35, 40);
         Reservation reservation = new Reservation("double",0, 4,true, 0);
-
         showRoom(room);
         showEmployee(employee);
         showReservation(reservation);
+
     }
+
+
+
 
     private static void showRoom(Room r) {
         System.out.println("Room: ");
         System.out.println("Numbers of Beds: " + r.getNumberOBeds() +
-         "\nAvailable: " + r.isAvailable());
+         "\nAvailable: " + r.isAvailable() + "\nNeed to be Clean: " + r.cleanRoom());
     }
     private static void showEmployee(Employee e) {
         System.out.println("\nEmployee: ");
-        System.out.println(e.getName() + " made " + e.getTotalPay());
+        System.out.println(e.getName() + " made " + e.getTotalPay() + "\nPunched in: " + e.getTime());
     }
 
     private static void showReservation( Reservation r) {
