8f0442180097f4661575a423fb7af51b777f60ab
Siry Siu <sewsiri@gmail.com>
2024-10-27 23:35:31 -0400

    process of making the ui and method to call user input

--- car-dealership/src/main/java/com/pluralsight/car/UserInterface.java
+++ car-dealership/src/main/java/com/pluralsight/car/UserInterface.java
@@ -1,69 +1,75 @@
 package com.pluralsight.car;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInterface {
 
-    public static void displayDealership() {
-        Scanner scanner = new Scanner(System.in);
-        boolean isrunning = true;
-        System.out.print("""
-                 Please choose an option:
-                 1. Price
-                 2. Make and Model
-                 3. Year
-                 4. Color
-                 5. Mileage
-                 6. Type
-                 7. All Vehicles
-                 8. Add Vehicles
-                 9. Remove Vehicles
-                 """);
+  private Dealership dealership;
 
-        String choice = scanner.nextLine();
-        while (isrunning) {
-            switch (choice) {
-                case "1":
-                    Dealership.getVehiclesByPrice();
-                    break;
-                case "2":
-                    Dealership.getVehiclesByMakeModel();
-                    break;
-                case "3":
-                    Dealership.getVehiclesByYear();
-                    break;
-                case "4":
-                    Dealership. getVehiclesByColor();
-                    break;
-                case "5":
-                    Dealership. getVehicleByMileage();
-                    break;
-                case "6":
-                    Dealership.getVehicleByType();
-                    break;
-                case "7":
-                    Dealership.displayInventory();
-                    break;
+  public UserInterface() {
 
-            } isrunning = false;
-        }
-    }
-
-    public static void display() {
-
-    }
-
+  }
     private void init() {
-
+      this.dealership = new DealershipFileManger().getDealership();
     }
 
-    private void displayVehicles() {
+    public void display() {
+      init();
+      boolean isRunning = true;
+      while (isRunning) {
+        menus();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        switch (input) {
+          case "1":
+            processGetByPriceRequest();
+            break;
+          case "x", "X":
+            isRunning = false;
+        }
 
+      }
+    }
+
+    private static void menus() {
+      System.out.println("""
+              Menu
+              Please Enter a Option:
+              [1] Vehicle By Price
+              [2] Vehicle By Make and Model
+              [3] Vehicle By Year
+              [4] Vehicle By Color
+              [5] Vehicle By Mileage 
+              [6] Vehicle By Type
+              [7] Show All
+              [8] Add Vehicle
+              [9] Remove Vehicle 
+              [x] Exit
+            
+              
+              """);
+    }
+
+
+
+
+
+    private void displayVehicles(ArrayList<Vehicle> vehicles) {
+      for (Vehicle vehicle: vehicles) {
+        System.out.println(vehicle);
+      }
+    }
+
+    private void processGetByPriceRequest() {
+    //  ArrayList<Vehicle> vehiclesByPrice = dealership.getVehiclesByPrice(specificPrice);
+    //  System.out.println(vehiclesByPrice);
     }
 
     public void processAllVehiclesRequest() {
 
     }
 
-    }
+  }
+
 
